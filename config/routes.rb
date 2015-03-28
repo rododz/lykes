8dafd68b9af346153675f81eb723269d32ffba3f
Maxencedouet <maxence.douet@gmail.com>
2015-03-28 20:08:24 +0100

    add omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
+
+# devise_scope :user do
+#   get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+#   get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+# end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
