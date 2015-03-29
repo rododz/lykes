7ac344f460cea9f27dec17fd8249f4f45346ae8b
TheoCarrive <theo.carrive@gmail.com>
2015-03-29 17:36:06 +0200

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable,  :omniauth_providers => [:facebook]
 
- 	def self.find_from_omniauth auth_hash
+  def self.find_from_omniauth auth_hash
     joins(:provider).where(providers: {token: auth_hash["credentials"]["token"], uid: auth_hash["uid"]}).first
-	end
+  end
 
   def self.create_from_omniauth auth_hash
     user = create(email: auth_hash['info']['email'], password: Devise.friendly_token[0,20])
